4e20cdd289a7dc48b51d0ed1856d49ca62a551e6
richardtam.ny <richardtam.ny@gmail.com>
2019-02-08 18:16:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -57,4 +57,8 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
+  for counter in 1..number_of_times do
+    puts phrase
+    counter += 1
+  end
 end
